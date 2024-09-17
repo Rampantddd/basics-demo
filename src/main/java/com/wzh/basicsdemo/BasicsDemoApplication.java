e9071f7256cef507a8a7bd40f0759c7ea0d502d5
Rampantddd <2454988619@qq.com>
2024-09-17 17:54:10 +0800

    初始化项目

---
+++ src/main/java/com/wzh/basicsdemo/BasicsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wzh.basicsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsDemoApplication.class, args);
+    }
+
+}
